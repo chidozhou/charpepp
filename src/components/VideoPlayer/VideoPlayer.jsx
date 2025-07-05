1d9b843ee6ce977588fff2d3411171530d5438db
chidozhou <chidochashegerald@gmail.com>
2025-07-05 11:06:18 +0200

    first commit

--- src/components/VideoPlayer/VideoPlayer.jsx
+++ src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,25 +1,25 @@
-import "./videoPlayer.css";
-import video from "../../assets/video.mp4";
-import { useRef } from "react";
+// import "./videoPlayer.css";
+// import video from "../../assets/video.mp4";
+// import { useRef } from "react";
 
-const VideoPlayer = ({ setPlayState, playState }) => {
-  const player = useRef(null);
+// const VideoPlayer = ({ setPlayState, playState }) => {
+//   const player = useRef(null);
 
-  const closePlayer = (e) => {
-    if (e.target === player.current) {
-      setPlayState(false);
-    }
-  };
+//   const closePlayer = (e) => {
+//     if (e.target === player.current) {
+//       setPlayState(false);
+//     }
+//   };
 
-  return (
-    <div
-      className={`video-player ${playState ? "" : "hide"}`}
-      ref={player}
-      onClick={closePlayer}
-    >
-      <video src={video} autoPlay muted controls></video>
-    </div>
-  );
-};
+//   return (
+//     <div
+//       className={`video-player ${playState ? "" : "hide"}`}
+//       ref={player}
+//       onClick={closePlayer}
+//     >
+//       <video src={video} autoPlay muted controls></video>
+//     </div>
+//   );
+// };
 
-export default VideoPlayer;
+// export default VideoPlayer;
